50ffe45c572b25169366de431e11a08e39d660a8
chrisWO92 <chrisworkingonline@gmail.com>
2022-09-12 09:48:11 -0300

    tests

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import styles from "./App.module.css";
+import { useEffect, useState } from "react";
+import { getPeople } from "./api/people.js"
 
-function App() {
+export function App() {
+
+  const [people, setPeople] = useState([]);
+  const [errorState, setErrorState] = useState({ hasError: false});
+
+  useEffect(() => {
+    getPeople()
+      .then((data => setPeople(data.results)))
+      .catch(handleError);
+  }, []);
+
+  const handleError = (err) => {
+    setErrorState({ hasError: true, message: err.message });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ul className={styles.App}>
+      {errorState.hasError && <div>{errorState.message}</div>}
+      {people.map((character) => (
+        <li key={character.name}>{character.name}</li>
+      ))}
+    </ul>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,51 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import data from "./data.json"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+/* We use render and screen functions from testing-library/react to start introducing myself to TDD aproach. Typing npm t in the terminal will open the test mode.  */
+describe("Star Wars APP", () => { 
+/*
+  it("Should show a list of characters including Luke Skywalker", () => {
+    render(<App />);
+    expect(screen.getByText("Luke Skywalker")).toBeInTheDocument();
+  });
+
+  it("should show a list of characters from a JSON file", () => {
+    render (<App />);
+    for (let character of data.results) {
+      expect(screen.getByText(character.name)).toBeInTheDocument();
+    }
+  });
+*/
+
+
+  /* It's used the beforeAll function as part of thes tests made on the API request */
+  /* We do this becuase we want to note when the code is making a fetch, to make it use the fetch method we create inside the people.js file, and not the native fetch methode */
+  beforeAll(() => jest.spyOn(window, 'fetch'));
+
+  it("should show a list of characters from the API", async () => {
+    /* We make a mock to know when the code is making a fetch */
+    window.fetch.mockResolvedValueOnce({
+      ok: true,
+      json: async () => data,
+    });
+    render(<App />);
+    /* We ask if it has been called 1 time */
+    expect(window.fetch).toHaveBeenCalledTimes(1);
+    /* We ask if it hass been called with this path */
+    expect(window.fetch).toHaveBeenCalledWith('http://swapi.dev/api/people/');
+
+    for (let character of data.results) {
+      expect(await screen.findByText(character.name)).toBeInTheDocument();
+    }
+  });
+
+  it("should show an error mesage when has a network error", async () => {
+    window.fetch.mockResolvedValueOnce(new Error("Network error"));
+
+    render(<App />);
+
+    expect(await screen.findByText("Network error")).toBeInTheDocument();
+  });
 });
